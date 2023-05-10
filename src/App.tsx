8163b7f66d57edb478b9dfcdfbb61886422be752
amitchongbang <achongbang@elementengineering.com.au>
2023-05-10 08:32:10 +0800

    Updated eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello World test</div>;
+  return (
+    <div>
+      <h1>React-TypeScript-starter-kit</h1>
+      <p>This is a react-typescript starter kit.</p>
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 
 import App from './App';
+
+import './assets/css/simple.css';
 
 const MOUNT_ROOT = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(MOUNT_ROOT);
